97f918bb6bb1d651bb594cf46f70cda328e07815
yasen911 <yasenarslan@gmail.com>
2020-08-16 18:29:46 +0200

    ilk commit from master

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -3,7 +3,9 @@ package git_project_branch;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("16:28 - master branchten ilk commit");
 
 	}
 
